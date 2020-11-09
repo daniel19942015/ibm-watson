70cb6dba05896af823c2b429d0ac0cd3907d8acf
SunriseSystem <daniel199257@gmail.com>
2020-11-09 11:07:35 -0300

    Update app.js
    Ok

--- ibm-cloud-api/app.js
+++ ibm-cloud-api/app.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const request = require("request");
 
 const endpoint = 'https://stream.watsonplatform.net/speech-to-text/api';
-const api = Buffer.from("apikey:zJOMyWvrT5n49l_oTawx8BB0FAHE9XVLMAeO0ITHwXQD").toString("base64");
+const api = Buffer.from("apikey:").toString("base64");
 const file = 'audio-file.flac'; 
 
 const options = {
